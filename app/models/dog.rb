b3b0a17299bff14df94f4eb62b103478dde27887
Laura Darlak <laura@polyporedesign.com>
2018-10-19 16:21:43 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,8 +1,15 @@
 class Dog
   attr_accessor :name, :breed, :age
 
-  def initialize(name, breed, age)
-    
+  @@all = []
+
+  def save
+    dog = Dog.new
+    @@all << dog
+  end
+
+  def self.all
+    @@all
   end
 
 
